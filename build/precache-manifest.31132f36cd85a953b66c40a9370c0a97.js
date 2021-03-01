21ff2a579419e52a4d28c910391cf8bbab25e34b
Jochum Sloot <67738459+Jsloot1986@users.noreply.github.com>
2021-03-01 17:09:33 +0100

    made a build folder

---
+++ build/precache-manifest.31132f36cd85a953b66c40a9370c0a97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ccf4a00113018e61f7b3bd68b28e9be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08acccf50bc76a31d77a",
+    "url": "/static/css/main.ec6308e8.chunk.css"
+  },
+  {
+    "revision": "643485f7ac2519ec1be8",
+    "url": "/static/js/2.bdcd465b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bdcd465b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08acccf50bc76a31d77a",
+    "url": "/static/js/main.33f61a59.chunk.js"
+  },
+  {
+    "revision": "4d3816f3982c9aa07060",
+    "url": "/static/js/runtime-main.df493174.js"
+  }
+]);
